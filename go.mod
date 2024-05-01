b33720829a71073e844f0de137ed5762592b9ec7
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-05-01 14:09:31 -0400

    feat: proto command parser

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tomassar/redis-go
 
 go 1.22.2
+
+require github.com/tidwall/resp v0.1.1 // indirect
---
+++ proto.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/tidwall/resp"
+)
+
+const (
+	CommandSET string = "SET"
+)
+
+type Command interface {
+}
+
+type SetCommand struct {
+	key, val string
+}
+
+func parseCommand(raw string) (Command, error) {
+	rd := resp.NewReader(bytes.NewBufferString(raw))
+
+	for {
+		v, _, err := rd.ReadValue()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Read %s\n", v.Type())
+		if v.Type() == resp.Array {
+			for _, value := range v.Array() {
+				switch value.String() {
+				case CommandSET:
+					if len(v.Array()) != 3 {
+						return nil, fmt.Errorf("invalid number of variables of SET command")
+					}
+					cmd := SetCommand{
+						key: v.Array()[1].String(),
+						val: v.Array()[2].String(),
+					}
+					return cmd, nil
+				}
+			}
+		}
+	}
+
+	return "", nil
+}
